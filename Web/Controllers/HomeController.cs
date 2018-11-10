6017391d8c59b5f7626e8fb2c61f67feec4a2918
haddadMoez <36275237+haddadMoez@users.noreply.github.com>
2018-11-11 00:57:28 +0100

    template integration(details in description)
    front office tempalte (home + login + register)
    back office tempalte  (dashboard)
    links works between pages done
    start with "http://localhost:2663/Home/Index" it will lead you to all other links

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -12,19 +12,30 @@ namespace Web.Controllers
         {
             return View();
         }
-
-        public ActionResult About()
+        public ActionResult Login()
         {
-            ViewBag.Message = "Your application description page.";
-
+            return View();
+        }
+        public ActionResult Register()
+        {
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+        /*
 
-            return View();
-        }
+           public ActionResult About()
+           {
+               ViewBag.Message = "Your application description page.";
+
+               return View();
+           }
+
+           public ActionResult Contact()
+           {
+               ViewBag.Message = "Your contact page.";
+
+               return View();
+           }
+        */
     }
 }
