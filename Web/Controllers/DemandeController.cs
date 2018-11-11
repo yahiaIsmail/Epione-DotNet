b4828d3b3e6c331adfb7f1adb7602f39affeadae
haddadMoez <36275237+haddadMoez@users.noreply.github.com>
2018-11-11 14:56:33 +0100

    fix

---
+++ Web/Controllers/DemandeController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class DemandeController : Controller
+    {
+        
+        // GET: Demande
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //Add demand
+        public ActionResult Demande()
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        /*
+         // GET: Demande/Details/5
+         public ActionResult Details(int id)
+         {
+             return View();
+         }
+
+         // GET: Demande/Create
+         public ActionResult Create()
+         {
+             return View();
+         }
+
+         // POST: Demande/Create
+         [HttpPost]
+         public ActionResult Create(FormCollection collection)
+         {
+             try
+             {
+                 // TODO: Add insert logic here
+
+                 return RedirectToAction("Index");
+             }
+             catch
+             {
+                 return View();
+             }
+         }
+
+         // GET: Demande/Edit/5
+         public ActionResult Edit(int id)
+         {
+             return View();
+         }
+
+         // POST: Demande/Edit/5
+         [HttpPost]
+         public ActionResult Edit(int id, FormCollection collection)
+         {
+             try
+             {
+                 // TODO: Add update logic here
+
+                 return RedirectToAction("Index");
+             }
+             catch
+             {
+                 return View();
+             }
+         }
+
+         // GET: Demande/Delete/5
+         public ActionResult Delete(int id)
+         {
+             return View();
+         }
+
+         // POST: Demande/Delete/5
+         [HttpPost]
+         public ActionResult Delete(int id, FormCollection collection)
+         {
+             try
+             {
+                 // TODO: Add delete logic here
+
+                 return RedirectToAction("Index");
+             }
+             catch
+             {
+                 return View();
+             }
+         }
+        */
+    }
+}
