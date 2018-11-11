b010f7cb61452ba7424e746a2c6d66d10553d093
haddadMoez <36275237+haddadMoez@users.noreply.github.com>
2018-11-11 15:14:18 +0100

    fix

--- Web/Controllers/DemandeController.cs
+++ Web/Controllers/DemandeController.cs
@@ -17,16 +17,10 @@ namespace Web.Controllers
         //Add demand
         public ActionResult Demande()
         {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
+           
+            
                 return View();
-            }
+            
         }
 
         /*
