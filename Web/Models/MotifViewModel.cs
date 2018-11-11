23a0aeba2234b1e6ed473dcb42a27bb6994087ab
ellouzeskander <skanderellouze@yahoo.fr>
2018-11-11 14:13:37 +0100

    motif

---
+++ Web/Models/MotifViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Models
+{
+    public class MotifViewModel
+    {
+        public MotifViewModel()
+        {
+            this.rdvs = new List<rdv>();
+        }
+
+        public int id { get; set; }
+        public string description { get; set; }
+        public Nullable<int> doctor_id { get; set; }
+        public virtual user user { get; set; }
+        public virtual ICollection<rdv> rdvs { get; set; }
+    }
+}
