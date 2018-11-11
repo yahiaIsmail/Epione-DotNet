1a9e7006c425b8fc856108e2e5c4d927cfc0bdaa
chaimaB <chaima.braiek@esprit.tn>
2018-11-11 15:29:02 +0100

    service consommation

---
+++ Web/Controllers/ConversationController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using Data.Models;
+
+namespace Web.Controllers
+{
+    public class ConversationController : Controller
+    {
+        // GET: Conversation
+        public ActionResult Index()
+        {
+            HttpClient Client = new HttpClient();
+            Client.BaseAddress = new Uri("http://localhost:2663/");
+            Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = Client.GetAsync("api/conversation").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                ViewBag.result = response.Content.ReadAsAsync<IEnumerable<conversation>>().Result;
+            }else
+            {
+                ViewBag.result = "error";
+            }
+            return View();
+        }
+        // GET: Conversation/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Conversation/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View("create");
+        }
+
+        // POST: Conversation/Create
+        [HttpPost]
+        public ActionResult Create(conversation conv)
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri("http://localhost:2663/");
+
+                client.PostAsJsonAsync<conversation>("api/conversation", conv).ContinueWith((postTask) => postTask.Result.EnsureSuccessStatusCode());
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Conversation/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Conversation/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Conversation/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Conversation/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
