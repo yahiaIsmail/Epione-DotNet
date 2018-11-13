fdfe7b314e2a3996e89fc7397ae45d1ee0d07985
haddadMoez <36275237+haddadMoez@users.noreply.github.com>
2018-11-13 12:20:34 +0100

    crud demand done (display not fixed yet)

--- Web/Controllers/DemandeController.cs
+++ Web/Controllers/DemandeController.cs
@@ -15,14 +15,36 @@ namespace Web.Controllers
         // GET: Demande
         public ActionResult Index()
         {
+            
             return View();
         }
+
+        public ActionResult allDemands()
+        {
+            HttpClient Client = new HttpClient();
+            Client.BaseAddress = new Uri("http://localhost:18080/");
+            Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = Client.GetAsync("JAVAEE-web/rest/admin/demandes").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                ViewBag.result = response.Content.ReadAsAsync<IEnumerable<demande>>().Result;
+            }
+            else
+            {
+                ViewBag.result = "error";
+            }
+            return View();
+        }
+
+
         //Add demand
 
         public ActionResult Demande()
         {
-       
-                return View();
+
+            
+            return View();
             
         }
         [HttpGet]
@@ -36,12 +58,21 @@ namespace Web.Controllers
         [HttpPost]
         public ActionResult CreateDemand(FormCollection collection)
         {
+            /*
+            
+
+            */
+
+
             demande d = new demande();
+
             d.email = collection["email"];
             d.firstName = collection["firstName"];
             d.lastName = collection["lastName"];
             d.speciality = collection["speciality"];
             d.state = collection["state"];
+
+
 
             System.Diagnostics.Debug.WriteLine(collection["email"]);
             System.Diagnostics.Debug.WriteLine(collection["firstName"]);
@@ -49,23 +80,86 @@ namespace Web.Controllers
             System.Diagnostics.Debug.WriteLine(collection["speciality"]);
             System.Diagnostics.Debug.WriteLine(collection["state"]);
 
-            try
-            {
+
+
+           // try
+         //   {
                 // TODO: Add insert logic here
+
                 HttpClient client = new HttpClient();
                 client.BaseAddress = new Uri("http://localhost:18080/");
-                // client.PostAsJsonAsync<DemandeViewModel>("Epione-web/rest/doctolib/ajoutDemande", demande).ContinueWith((postTask) => postTask.Result.EnsureSuccessStatusCode());
-                client.PostAsJsonAsync<demande>("JAVAEE-web/rest/admin/adddemande", d)
-               .ContinueWith((postTask) => postTask.Result.EnsureSuccessStatusCode());
 
-
-                return RedirectToAction("Demande");
+                HttpResponseMessage response = client.PostAsJsonAsync<demande>("JAVAEE-web/rest/admin/adddemande", d).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    client.PostAsJsonAsync<demande>("JAVAEE-web/rest/admin/adddemande", d);
+                    // .ContinueWith((postTask) => postTask.Result.EnsureSuccessStatusCode());
+               
+                    return RedirectToAction("Demande");
+                }
+                else
+                {
+               
+                ViewBag.result = "error";
+              //  System.Diagnostics.Debug.WriteLine(ViewBag.result);
             }
-            catch
-            {
+
+
+           // client.PostAsJsonAsync<demande>("JAVAEE-web/rest/admin/adddemande", d);
+                // .ContinueWith((postTask) => postTask.Result.EnsureSuccessStatusCode());
+
+                
+
+            //    return RedirectToAction("Demande");
+
+          //  }
+            //catch
+            //{
                 return View();
-            }            
+         //   }
+
         }
+
+        public ActionResult Delete(String email)
+        {
+            System.Diagnostics.Debug.WriteLine("******* email ********");
+            System.Diagnostics.Debug.WriteLine(email);
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Delete(String email, FormCollection collection)
+        {
+
+            System.Diagnostics.Debug.WriteLine("******* email ********");
+            System.Diagnostics.Debug.WriteLine(email);
+
+            demande d = new demande();
+
+            d.email = email;
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:18080/");
+
+            HttpResponseMessage response = client.PostAsJsonAsync<demande>("JAVAEE-web/rest/admin/deletedemande", d).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                client.PostAsJsonAsync<demande>("JAVAEE-web/rest/admin/deletedemande", d);
+                // .ContinueWith((postTask) => postTask.Result.EnsureSuccessStatusCode());
+
+                return RedirectToAction("allDemands");
+            }
+            else
+            {
+
+                ViewBag.result = "error";
+            }
+                return View();
+
+
+        }
+
+
 
         /*
          // GET: Demande/Details/5
