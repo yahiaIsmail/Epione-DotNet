0ab4aa1c6912a679bd5e5a773b772735b3005465
yahia_ismail <ismail.yahia@esprit.tn>
2019-01-17 16:36:58 +0100

    UPDATE MEDICALPATH VFINAL

---
+++ Web/Excel/DoctorsExcel.cs
@@ -0,0 +1,211 @@
+﻿using Web.Models;
+using OfficeOpenXml;
+using OfficeOpenXml.Style;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web.Excel
+{
+    public class DoctorsExcel
+    {
+        int rowIndex = 2;
+        ExcelRange cell;
+        ExcelFill fill;
+        Border border;
+
+        public Byte[] GenerateExcel(List<DoctorViewModel> docs)
+        {
+            using (var excelPackage = new ExcelPackage())
+            {
+                excelPackage.Workbook.Properties.Author = " Ismail Epione";
+                excelPackage.Workbook.Properties.Title = "List Of doctors";
+                var sheet = excelPackage.Workbook.Worksheets.Add("Mandate Excel");
+                sheet.Name = " Doctors In Patient medical Path";
+                sheet.Column(2).Width = 10;
+                sheet.Column(3).Width = 30;
+                sheet.Column(4).Width = 30;
+                sheet.Column(5).Width = 30;
+                sheet.Column(6).Width = 30;
+                sheet.Column(7).Width = 30;
+
+                #region Report Header
+                sheet.Cells[rowIndex, 2, rowIndex, 4].Merge = true;
+                cell = sheet.Cells[rowIndex, 2];
+                cell.Value = "DeepOr Epione";
+                cell.Style.Font.Bold = true;
+                cell.Style.Font.Size = 20;
+                cell.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                rowIndex = rowIndex + 1;
+
+                sheet.Cells[rowIndex, 2, rowIndex, 4].Merge = true;
+                cell = sheet.Cells[rowIndex, 2];
+
+                cell.Value = "List Doctors";
+                cell.Style.Font.Bold = true;
+                cell.Style.Font.Size = 15;
+                cell.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                rowIndex = rowIndex + 2;
+                #endregion
+
+
+                #region Table header
+                cell = sheet.Cells[rowIndex, 2];
+                cell.Value = "#";
+                cell.Style.Font.Bold = true;
+                cell.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                cell.Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+                fill = cell.Style.Fill;
+                fill.PatternType = ExcelFillStyle.Solid;
+                fill.BackgroundColor.SetColor(System.Drawing.Color.White);
+                border = cell.Style.Border;
+                border.Bottom.Style = border.Top.Style = border.Left.Style = border.Right.Style = ExcelBorderStyle.Thin;
+
+                cell = sheet.Cells[rowIndex, 3];
+                cell.Value = "Name & Surname";
+                cell.Style.Font.Bold = true;
+                cell.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                cell.Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+                fill = cell.Style.Fill;
+                fill.PatternType = ExcelFillStyle.Solid;
+                fill.BackgroundColor.SetColor(System.Drawing.Color.LightGray);
+                border = cell.Style.Border;
+                border.Bottom.Style = border.Top.Style = border.Left.Style = border.Right.Style = ExcelBorderStyle.Thin;
+
+                cell = sheet.Cells[rowIndex, 4];
+                cell.Value = "Email";
+                cell.Style.Font.Bold = true;
+                cell.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                cell.Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+                fill = cell.Style.Fill;
+                fill.PatternType = ExcelFillStyle.Solid;
+                fill.BackgroundColor.SetColor(System.Drawing.Color.LightGray);
+                border = cell.Style.Border;
+                border.Bottom.Style = border.Top.Style = border.Left.Style = border.Right.Style = ExcelBorderStyle.Thin;
+
+                cell = sheet.Cells[rowIndex, 5];
+                cell.Value = "Phone Number";
+                cell.Style.Font.Bold = true;
+                cell.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                cell.Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+                fill = cell.Style.Fill;
+                fill.PatternType = ExcelFillStyle.Solid;
+                fill.BackgroundColor.SetColor(System.Drawing.Color.LightGray);
+                border = cell.Style.Border;
+                border.Bottom.Style = border.Top.Style = border.Left.Style = border.Right.Style = ExcelBorderStyle.Thin;
+
+                cell = sheet.Cells[rowIndex, 6];
+                cell.Value = "speciality";
+                cell.Style.Font.Bold = true;
+                cell.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                cell.Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+                fill = cell.Style.Fill;
+                fill.PatternType = ExcelFillStyle.Solid;
+                fill.BackgroundColor.SetColor(System.Drawing.Color.LightGray);
+                border = cell.Style.Border;
+                border.Bottom.Style = border.Top.Style = border.Left.Style = border.Right.Style = ExcelBorderStyle.Thin;
+
+                cell = sheet.Cells[rowIndex, 7];
+                cell.Value = "tarif";
+                cell.Style.Font.Bold = true;
+                cell.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                cell.Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+                fill = cell.Style.Fill;
+                fill.PatternType = ExcelFillStyle.Solid;
+                fill.BackgroundColor.SetColor(System.Drawing.Color.LightGray);
+                border = cell.Style.Border;
+                border.Bottom.Style = border.Top.Style = border.Left.Style = border.Right.Style = ExcelBorderStyle.Thin;
+
+                rowIndex = rowIndex + 1;
+
+                #endregion
+
+                #region Table body
+                int serialNumber = 1;
+                if (docs.Count > 0)
+                {
+                    foreach (DoctorViewModel doc in docs)
+                    {
+                        cell = sheet.Cells[rowIndex, 2];
+                        cell.Value = serialNumber++.ToString();
+
+                        cell.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                        cell.Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+                        fill = cell.Style.Fill;
+                        fill.PatternType = ExcelFillStyle.Solid;
+                        fill.BackgroundColor.SetColor(System.Drawing.Color.White);
+                        border = cell.Style.Border;
+                        border.Bottom.Style = border.Top.Style = border.Left.Style = border.Right.Style = ExcelBorderStyle.Thin;
+
+                        cell = sheet.Cells[rowIndex, 3];
+                        cell.Value = doc.firstName +" "+doc.lastName;
+                        cell.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                        cell.Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+                        fill = cell.Style.Fill;
+                        fill.PatternType = ExcelFillStyle.Solid;
+                        fill.BackgroundColor.SetColor(System.Drawing.Color.White);
+                        border = cell.Style.Border;
+                        border.Bottom.Style = border.Top.Style = border.Left.Style = border.Right.Style = ExcelBorderStyle.Thin;
+
+                        cell = sheet.Cells[rowIndex, 4];
+                        cell.Value = doc.email;
+                        cell.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                        cell.Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+                        fill = cell.Style.Fill;
+                        fill.PatternType = ExcelFillStyle.Solid;
+                        fill.BackgroundColor.SetColor(System.Drawing.Color.White);
+                        border = cell.Style.Border;
+                        border.Bottom.Style = border.Top.Style = border.Left.Style = border.Right.Style = ExcelBorderStyle.Thin;
+
+                        cell = sheet.Cells[rowIndex, 5];
+                        cell.Value = doc.phoneNumber;
+                        cell.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                        cell.Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+                        fill = cell.Style.Fill;
+                        fill.PatternType = ExcelFillStyle.Solid;
+                        fill.BackgroundColor.SetColor(System.Drawing.Color.White);
+                        border = cell.Style.Border;
+                        border.Bottom.Style = border.Top.Style = border.Left.Style = border.Right.Style = ExcelBorderStyle.Thin;
+
+                        cell = sheet.Cells[rowIndex, 6];
+                        cell.Value = doc.speciality;
+                        cell.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                        cell.Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+                        fill = cell.Style.Fill;
+                        fill.PatternType = ExcelFillStyle.Solid;
+                        fill.BackgroundColor.SetColor(System.Drawing.Color.White);
+                        border = cell.Style.Border;
+                        border.Bottom.Style = border.Top.Style = border.Left.Style = border.Right.Style = ExcelBorderStyle.Thin;
+
+                        cell = sheet.Cells[rowIndex, 7];
+                        cell.Value = doc.tariff; ;
+                        cell.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+                        cell.Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+                        fill = cell.Style.Fill;
+                        fill.PatternType = ExcelFillStyle.Solid;
+                        fill.BackgroundColor.SetColor(System.Drawing.Color.White);
+                        border = cell.Style.Border;
+                        border.Bottom.Style = border.Top.Style = border.Left.Style = border.Right.Style = ExcelBorderStyle.Thin;
+
+
+                        rowIndex = rowIndex + 1;
+
+                    }
+
+
+                }
+                #endregion
+                return excelPackage.GetAsByteArray();
+
+
+
+
+            }
+
+        }
+
+
+
+    }
+}
